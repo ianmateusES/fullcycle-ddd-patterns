b2e4f7951975929d2734d822db01be3656053343
ianmateusES <estudo.ianmateus@gmail.com>
2024-11-05 08:48:27 -0300

    feat: creating customer event domain

---
+++ src/domain/event/customer/customer-address-changed.event.ts
@@ -0,0 +1,12 @@
+import { Customer } from "../../entities";
+import { EventInterface } from "../@shared";
+
+export class CustomerAddressChangedEvent implements EventInterface {
+  data_time_occurred: Date;
+  event_data: Customer;
+
+  constructor(eventData: Customer) {
+    this.data_time_occurred = new Date();
+    this.event_data = eventData;
+  }
+}
---
+++ src/domain/event/customer/handler/index.ts
@@ -0,0 +1,3 @@
+export * from "./send-console-log-1-when-customer-is-created.handler";
+export * from "./send-console-log-2-when-customer-is-created.handler";
+export * from "./send-console-log-when-address-has-changed.handler";
